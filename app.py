726af008a90ad59106934e7dbda33f6241007b93
anuprshetty <anuprshetty@gmail.com>
2018-09-08 09:42:39 +0530

    attached blueprints to app

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask
-from blueprints.kandapadya import kandapadya_bp
-
+from blueprints import home_bp
 
 app = Flask(__name__)
-app.register_blueprint(kandapadya_bp)
 
+app.register_blueprint(home_bp)
 
 if __name__ == "__main__":
     app.run(debug=True)
