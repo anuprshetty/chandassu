e5c5bb313d30ea1afe3a1ec803ccbf6ef1bb4416
anuprshetty <anuprshetty@gmail.com>
2018-10-03 11:12:52 +0530

    routing for yati

--- blueprints/yati.py
+++ blueprints/yati.py
@@ -7,5 +7,12 @@ yati_bp = Blueprint(
 
 @yati_bp.route("/")
 def yati():
-    content = {"title": "Yati"}
+    content = {
+        "title": "Yati",
+        "heading": "ಯತಿ",
+        "footer": {
+            "previous": "home_bp.home",
+            "next": "home_bp.home",
+        },
+    }
     return render_template("yati.html", content=content)
