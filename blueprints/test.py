3bb689a83eac5c845c4803745c5dd40ab8c3838c
anuprshetty <anuprshetty@gmail.com>
2018-10-16 04:26:53 +0530

    test and result blueprints and templates structure

---
+++ blueprints/test.py
@@ -0,0 +1,31 @@
+from flask import Blueprint, render_template
+
+test_bp = Blueprint(
+    "test_bp", __name__, url_prefix="test", template_folder="../templates/test"
+)
+
+
+@test_bp.route("/")
+def test():
+    content = {
+        "title": "Test",
+        "heading": "ಪರೀಕ್ಷಿಸಿ",
+        "footer": {
+            "previous": "home_bp.home",
+            "next": "home_bp.home",
+        },
+    }
+    return render_template("test.html", content=content)
+
+
+@test_bp.route("/result")
+def result():
+    content = {
+        "title": "Result",
+        "heading": "ನಿಮ್ಮ ಪದ್ಯದ ಛಂದಸ್ಸಿನ ಮಾಹಿತಿ",
+        "footer": {
+            "previous": "home_bp.home",
+            "next": "home_bp.home",
+        },
+    }
+    return render_template("result.html", content=content)
