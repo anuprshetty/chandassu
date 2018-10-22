5812b45f4088893e162754717cc4f601ce27b4e8
anuprshetty <anuprshetty@gmail.com>
2018-10-23 03:07:06 +0530

    filter kannada symbols

--- console/poem.py
+++ console/poem.py
@@ -24,3 +24,31 @@ class Poem:
     def letters(cls):
         return cls._poem_kannada_letters
 
+    @classmethod
+    def _filter_kannada_symbols(cls):
+        poem = ""
+
+        # remove all non-kannada unicode characters.
+        for letter in cls._poem:
+            letter_unicode = ord(letter)
+            if (
+                Constant.kannada_unicode_range.get("start")
+                <= letter_unicode
+                <= Constant.kannada_unicode_range.get("end")
+                or letter == " "
+                or letter == "\n"
+            ):
+                poem += letter
+
+        poem = re.sub(r" +", " ", poem)  # replace multiple spaces with single space.
+        poem = re.sub(r"\n+", "\n", poem)  # replace multiple '\n' with single '\n'.
+        poem_lines = poem.split("\n")
+        poem_lines = [
+            poem_line.strip() for poem_line in poem_lines
+        ]  # strip the left and right spaces in each line.
+        poem_lines = [
+            poem_line for poem_line in poem_lines if poem_line != ""
+        ]  # remove empty lines.
+
+        cls._poem_kannada_symbols = [list(poem_line) for poem_line in poem_lines]
+
