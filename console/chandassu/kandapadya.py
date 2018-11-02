304531e49282c1f4840df8e1050225d00d0a68a7
anuprshetty <anuprshetty@gmail.com>
2018-11-02 10:19:10 +0530

    Kandapadya indentification logic implemented

--- console/chandassu/kandapadya.py
+++ console/chandassu/kandapadya.py
@@ -17,3 +17,37 @@ class Kandapadya(Chandassu):
 
     _gana_should_not_occur = "121"
 
+    @classmethod
+    def identified(cls):
+        if len(cls._prastara_values) == 0:
+            return False
+
+        if len(cls._prastara_values) != cls._total_lines:
+            return False
+
+        if (
+            sum(cls._prastara_values[0]) != 12
+            or sum(cls._prastara_values[2]) != 12
+            or sum(cls._prastara_values[1]) != 20
+            or sum(cls._prastara_values[3]) != 20
+        ):
+            return False
+
+        if not cls._pattern_matched(
+            cls._total_ganas_per_short_line,
+            cls._total_ganas_per_long_line,
+            cls._matras_count,
+            cls._matras_count,
+            cls._first_long_line_index,
+            cls._last_long_line_index,
+            has_extra_guru=False,
+        ):
+            return False
+
+        if cls._special_conditions_met():
+            cls._form_ganas(cls._matras_count, cls._matras_count)
+
+            return True
+        else:
+            return False
+
