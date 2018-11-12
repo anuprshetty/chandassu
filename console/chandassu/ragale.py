afea11150d26ce37252cb8ee3cce76c73d68dc5c
anuprshetty <anuprshetty@gmail.com>
2018-11-12 11:26:59 +0530

    Ragale indentification logic implemented

--- console/chandassu/ragale.py
+++ console/chandassu/ragale.py
@@ -12,3 +12,35 @@ class Ragale(Chandassu):
     _gana_should_not_occur_1 = None
     _gana_should_not_occur_2 = None
 
+    @classmethod
+    def identified(cls):
+        if len(cls._prastara_values) == 0:
+            return False
+
+        if (
+            cls._scenarios == None
+            or cls._gana_should_not_occur_1 == None
+            or cls._gana_should_not_occur_2 == None
+        ):
+            return False
+
+        if len(cls._prastara_values) < cls._min_lines:
+            return False
+
+        for scenario in cls._scenarios:
+            if cls._check_for(scenario):
+                first_alternative_matras_count = scenario.get(
+                    "first_alternative_matras_count"
+                )
+                second_alternative_matras_count = scenario.get(
+                    "second_alternative_matras_count"
+                )
+
+                cls._form_ganas(
+                    first_alternative_matras_count, second_alternative_matras_count
+                )
+
+                return True
+
+        return False
+
