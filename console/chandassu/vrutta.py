a593eb75184b720c5c77be1170c3f24ab2d49336
anuprshetty <anuprshetty@gmail.com>
2019-01-20 03:34:14 +0530

    Vrutta class defined

---
+++ console/chandassu/vrutta.py
@@ -0,0 +1,10 @@
+from .chandassu import Chandassu
+from constant import Constant
+
+
+class Vrutta(Chandassu):
+    _name = Constant.chandassu_names.get("vrutta").get("vrutta")
+    _total_lines = 4
+
+    _line_pattern = None
+
