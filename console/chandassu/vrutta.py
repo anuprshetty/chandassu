350f99bfd0fca65783fba64848834ebb9a5c52af
anuprshetty <anuprshetty@gmail.com>
2019-01-20 09:34:56 +0530

    Vrutta indentification logic implemented

--- console/chandassu/vrutta.py
+++ console/chandassu/vrutta.py
@@ -8,3 +8,22 @@ class Vrutta(Chandassu):
 
     _line_pattern = None
 
+    @classmethod
+    def identified(cls):
+        if len(cls._prastara_values) == 0:
+            return False
+
+        if cls._line_pattern == None:
+            return False
+
+        if len(cls._prastara_values) == cls._total_lines:
+            for prastara_line in cls._prastara_values:
+                prastara_line_pattern = "".join(str(value) for value in prastara_line)
+                if prastara_line_pattern != cls._line_pattern:
+                    return False
+
+            cls._form_ganas()
+            return True
+        else:
+            return False
+
